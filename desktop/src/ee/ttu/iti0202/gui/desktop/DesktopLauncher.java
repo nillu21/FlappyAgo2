ae02b008d7c91b19223c5c28d30b604f5602b913
nilst <nitirs@ttu.ee>
2019-05-14 11:41:08 +0300

    Initial commit

---
+++ desktop/src/ee/ttu/iti0202/gui/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ee.ttu.iti0202.gui.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ee.ttu.iti0202.gui.FlappyAgo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyAgo(), config);
+	}
+}
